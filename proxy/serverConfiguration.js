5bdb392d55f8713e3548ca9ac4855575625deb59
Dror Tirosh <dror@tabookey.com>
2019-05-27 17:02:04 +0300

    add production app cert to proxy configuration

--- proxy/serverConfiguration.js
+++ proxy/serverConfiguration.js
@@ -1,7 +1,9 @@
 module.exports = {
 	allowedSigs: [
-		"TwsoRSoLU3adBbfTtNAqv8eARKTj4dsxMxxCsyabv8A=", // Dror dev key
-		"dvwkoaPmOYep2kn/mW+7CC883kWkcj4RySEs6lJZECU=" // Alex dev key
+		'+/+9FD3CNykOqfhN0rFLk5AuZyIgx3e/MeWbsXUc2GU=', // Google Play version certificate
+
+		"TwsoRSoLU3adBbfTtNAqv8eARKTj4dsxMxxCsyabv8A=", // Dror dev key - REMOVE FROM PRODUCTION
+		"dvwkoaPmOYep2kn/mW+7CC883kWkcj4RySEs6lJZECU=", // Alex dev key - REMOVE FROM PRODUCTION
 	],
 	secretString: "hello!"
 }
