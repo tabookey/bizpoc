effefef269ee4deeffe63ef243f031fd5e27a3ac
shahafn <shahaflol@gmail.com>
2019-06-13 01:45:29 +0300

    Creating utils.sh

--- provision/keygen/src/generate_master_key.sh
+++ provision/keygen/src/generate_master_key.sh
@@ -1,51 +1,6 @@
 #!/bin/bash -e
 
-workdir="/tmp/bizpoc/"
-declare tmppass
-
-# helper functions start
-
-# $1 is participant's name
-# $2 is filename
-function write_to_usb {
-[ $1 ]
-[ $2 ]
-
-echo "Please plug in $1's usb device"
-while [ ! -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Waiting for usb device...";
-done
-sleep 2
-echo "Writing file to $1's usb device...";
-targetfile=`mount|grep sdb1|cut -d " " -f3`
-[ -f ${workdir}/$2 ]
-[ -d $targetfile ]
-dd if=${workdir}/$2 of=$targetfile/$2 && sync
-dd if=${workdir}/salt of=$targetfile/salt && sync
-echo "Done.";
-while [ -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Please take out usb device...";
-done
-}
-
-# $1 is participant's name
-function read_password {
-[ $1 ]
-read -s -p "Enter $1's Password: " tmppass
-echo ""
-read -s -p "Re-enter Password: " tmppass2
-echo ""
-if [ "${tmppass}" != "${tmppass2}" ]; then
-    echo "Wrong password. Exiting."
-    exit 1
-fi
-
-}
-
-# helper functions end
-
+. `dirname $0`/utils.sh
 # Getting Liraz's password
 read_password Liraz
 firstpass=$tmppass
--- provision/keygen/src/restore_participant.sh
+++ provision/keygen/src/restore_participant.sh
@@ -1,93 +1,23 @@
 #!/bin/bash -e
 
-workdir="/tmp/bizpoc/"
-declare tmppass
-
-# helper functions start
-
-# $1 is participant's name
-# $2 is filename
-function read_from_usb {
-[ "$1" ]
-[ $2 ]
-echo "Please plug in $1's usb device"
-while [ ! -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Waiting for usb device...";
-done
-sleep 2
-echo "Writing file from $1's usb device to $workdir";
-srcpath=`mount|grep sdb1|cut -d " " -f3`
-[ -f $srcpath/$2 ]
-[ -d ${workdir} ]
-dd if=$srcpath/$2 of=${workdir}/$2 && sync
-dd if=${workdir}/salt of=$srcpath/salt && sync
-echo "Done.";
-while [ -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Please take out usb device...";
-done
-
-}
-
-# $1 is participant's name
-# $2 is filename
-function write_to_usb {
-[ $1 ]
-[ $2 ]
-
-echo "Please plug in $1's usb device"
-while [ ! -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Waiting for usb device...";
-done
-sleep 2
-echo "Writing file to $1's usb device...";
-targetfile=`mount|grep sdb1|cut -d " " -f3`
-[ -f ${workdir}/$2 ]
-[ -d $targetfile ]
-dd if=${workdir}/$2 of=$targetfile/$2 && sync
-dd if=${workdir}/salt of=$targetfile/salt && sync
-echo "Done.";
-while [ -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Please take out usb device...";
-done
-}
-
-# $1 is participant's name
-function read_password {
-[ "$1" ]
-read -s -p "Enter $1's Password: " tmppass
-echo ""
-read -s -p "Re-enter Password: " tmppass2
-echo ""
-if [ "${tmppass}" != "${tmppass2}" ]; then
-    echo "Wrong password. Exiting."
-    exit 1
-fi
-
-}
-
-# helper functions end
-
+. `dirname $0`/utils.sh
 # $1 is filename of first participant
 # $2 is filename of second participant
 # $3 is name of new restored participant (Liraz, Yoav, Adi etc.)
-[ $1 ]
-[ $2 ]
-[ $3 ]
+[ "$1" ]
+[ "$2" ]
+[ "$3" ]
 
 mkdir -p ${workdir}
-read_from_usb "first participant" $1
+read_from_usb "first participant" "$1"
 read_password "first participant"
 firstpass=${tmppass}
 
-read_from_usb "second participant" $2
+read_from_usb "second participant" "$2"
 read_password "second participant"
 secondpass=${tmppass}
 
-read_password $3
+read_password "$3"
 thirdpass=${tmppass}
 
 # Restore participant in node script, saving it to $workdir
