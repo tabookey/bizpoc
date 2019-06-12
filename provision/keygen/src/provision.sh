678dc999249c347b2e31d55c36a6043944b2d686
shahafn <shahaflol@gmail.com>
2019-06-12 15:39:19 +0300

    continue wit restore_participant.sh, improving provision.sh

--- provision/keygen/src/provision.sh
+++ provision/keygen/src/provision.sh
@@ -1,83 +1,70 @@
 #!/bin/bash -e
 
-# Getting Liraz's password
-read -s -p "Enter Liraz's Password: " lirazpass
+workdir="/tmp/bizpoc/"
+declare tmppass
+
+# helper functions start
+
+# $1 is participant's name
+# $2 is filename
+function write_to_usb {
+[ $1 ]
+[ $2 ]
+
+echo "Please plug in $1's usb device"
+while [ ! -b /dev/sdb1 ] ; do
+    sleep 3;
+    echo "Waiting for usb device...";
+done
+sleep 2
+echo "Writing file to $1's usb device...";
+targetfile=`mount|grep sdb1|cut -d " " -f3`
+[ -f ${workdir}/$2 ]
+[ -d $targetfile ]
+dd if=${workdir}/$2 of=$targetfile/$2 && sync
+dd if=${workdir}/salt of=$targetfile/salt && sync
+echo "Done.";
+while [ -b /dev/sdb1 ] ; do
+    sleep 3;
+    echo "Please take out usb device...";
+done
+}
+
+# $1 is participant's name
+function read_password {
+[ $1 ]
+read -s -p "Enter $1's Password: " tmppass
 echo ""
-read -s -p "Re-enter Password: " lirazpass2
+read -s -p "Re-enter Password: " tmppass2
 echo ""
-if [ "${lirazpass}" != "${lirazpass2}" ]; then
+if [ "${tmppass}" != "${tmppass2}" ]; then
     echo "Wrong password. Exiting."
     exit 1
 fi
+
+}
+
+# helper functions end
+
+# Getting Liraz's password
+read_password Liraz
+lirazpass=$tmppass
 
 # Getting Yoav's password
-read -s -p "Enter Yoav's Password: " yoavpass
-echo ""
-read -s -p "Re-enter Password: " yoavpass2 
-echo ""
-if [ "${yoavpass}" != "${yoavpass2}" ]; then
-    echo "Wrong password. Exiting."
-    exit 1
-fi
+read_password Yoav
+yoavpass=$tmppass
 
 # Getting Adi's password
-read -s -p "Enter Adi's Password: " adipass
-echo ""
-read -s -p "Re-enter Password: " adipass2 
-echo ""
-if [ "${adipass}" != "${adipass2}" ]; then
-    echo "Wrong password. Exiting."
-    exit 1
-fi
-#echo $lirazpass $yoavpass $adipass
+read_password Adi
+adipass=$tmppass
+
+echo $lirazpass $yoavpass $adipass
 
 # Generating bitcoin keypair in node script
 export lirazpass yoavpass adipass
-workdir="/tmp/bizpoc/"
 time node src/js/generate_bitcoin_keypair.js -g -d ${workdir}
 
 # writing encrypted seed of the bitcoin keypair to participants' usb devices
-echo "Please plug in Liraz's usb device"
-while [ ! -b /dev/sdb1 ] ; do 
-    sleep 3; 
-    echo "Waiting for usb device..."; 
-done
-sleep 2
-echo "Writing file to Liraz's usb device...";
-targetfile=`mount|grep sdb1|cut -d " " -f3`
-dd if=${workdir}/mnemonicA of=$targetfile/mnemonicA && sync
-dd if=${workdir}/salt of=$targetfile/salt && sync
-echo "Done. Please switch to Yoav's usb device";
-while [ -b /dev/sdb1 ] ; do
-    sleep 3; 
-    echo "Please change usb device..."; 
-done
-
-echo "Please plug in Yoav's usb device"
-while [ ! -b /dev/sdb1 ] ; do 
-    sleep 3; 
-    echo "Waiting for usb device..."; 
-done
-sleep 2
-echo "Writing file to Yoav's usb device..."; 
-targetfile=`mount|grep sdb1|cut -d " " -f3`
-dd if=${workdir}/mnemonicB of=$targetfile/mnemonicB && sync
-dd if=${workdir}/salt of=$targetfile/salt && sync
-echo "Done. Please switch to Adi's usb device";
-while [ -b /dev/sdb1 ] ; do
-    sleep 3;
-    echo "Please change usb device...";
-done
-
-while [ ! -b /dev/sdb1 ] ; do 
-    sleep 3; 
-    echo "Waiting for usb device..."; 
-done
-sleep 2
-echo "Writing file to Adi's usb device..."; 
-targetfile=`mount|grep sdb1|cut -d " " -f3`
-dd if=${workdir}/mnemonicC of=$targetfile/mnemonicC && sync
-dd if=${workdir}/salt of=$targetfile/salt && sync
-echo "Done.";
-
-
+write_to_usb Liraz mnemonicA
+write_to_usb Yoav mnemonicB
+write_to_usb Adi mnemonicC
