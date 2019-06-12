09b11909153209313a4ed6747c6e3c7290a0032b
shahafn <shahaflol@gmail.com>
2019-06-12 03:16:37 +0300

    Adding recovery of lost participant

--- provision/keygen/src/provision.sh
+++ provision/keygen/src/provision.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+
+# Getting Liraz's password
 read -s -p "Enter Liraz's Password: " lirazpass
 echo ""
 read -s -p "Re-enter Password: " lirazpass2
@@ -8,6 +10,7 @@ if [ "${lirazpass}" != "${lirazpass2}" ]; then
     exit 1
 fi
 
+# Getting Yoav's password
 read -s -p "Enter Yoav's Password: " yoavpass
 echo ""
 read -s -p "Re-enter Password: " yoavpass2 
@@ -17,6 +20,7 @@ if [ "${yoavpass}" != "${yoavpass2}" ]; then
     exit 1
 fi
 
+# Getting Adi's password
 read -s -p "Enter Adi's Password: " adipass
 echo ""
 read -s -p "Re-enter Password: " adipass2 
@@ -26,7 +30,48 @@ if [ "${adipass}" != "${adipass2}" ]; then
     exit 1
 fi
 #echo $lirazpass $yoavpass $adipass
-#echo $lirazpass2 $yoavpass2 $adipass2
-export lirazpass yoavpass adipass
 
-time node src/js/generate_bitcoin_keypair.js --restore-participant
+# Generating bitcoin keypair in node script
+export lirazpass yoavpass adipass
+workdir="/tmp/bizpoc/"
+time node src/js/generate_bitcoin_keypair.js -g -d ${workdir}
+
+# writing encrypted seed of the bitcoin keypair to participants' usb devices
+echo "Please plug in Liraz's usb device"
+while [ ! -b /dev/sdb1 ] ; do 
+    sleep 3; 
+    echo "Waiting for usb device..."; 
+done
+echo "Writing file to Liraz's usb device..."; 
+dd if=${workdir}/mnemonicA of=/dev/sdb1 && sync
+dd if=${workdir}/salt of=/dev/sdb1 && sync
+echo "Done. Please switch to Yoav's usb device";
+while [ -b /dev/sdb1 ] ; do
+    sleep 3; 
+    echo "Please change usb device..."; 
+done
+
+echo "Please plug in Yoav's usb device"
+while [ ! -b /dev/sdb1 ] ; do 
+    sleep 3; 
+    echo "Waiting for usb device..."; 
+done
+echo "Writing file to Yoav's usb device..."; 
+dd if=${workdir}/mnemonicB of=/dev/sdb1 && sync
+dd if=${workdir}/salt of=/dev/sdb1 && sync
+echo "Done. Please switch to Adi's usb device";
+while [ -b /dev/sdb1 ] ; do
+    sleep 3;
+    echo "Please change usb device...";
+done
+
+while [ ! -b /dev/sdb1 ] ; do 
+    sleep 3; 
+    echo "Waiting for usb device..."; 
+done
+echo "Writing file to Adi's usb device..."; 
+dd if=${workdir}/mnemonicC of=/dev/sdb1 && sync
+dd if=${workdir}/salt of=/dev/sdb1 && sync
+echo "Done.";
+
+
